e03f87c11ead9ce262b1c92b28ee773edc953384
HybridDog <adresse_test@t-online.de>
2013-08-10 20:06:21 +0200

    update

--- init.lua
+++ init.lua
@@ -1,30 +1,35 @@
---------------------------------------Rotten Papyrus 0.5----------------------------------------
+--------------------------------------Rotten Papyrus----------------------------------------
 --Textures (edited with gimp) from gamiano.de
 --nodebox from 3dforniture
 
+local t1 = os.clock()
+local PAPYRUS_INTERVALL = 600
+local PAPYRUS_CHANCE = 10
+
 local PAPYRUS_NODEBOX = {
-			type = "fixed",
-			fixed = {
-				--papyrus 1
-				{-0.03-0.1,-0.5,-0.03-0.1, 0.03-0.1,0.5,0.03-0.1},
-				{-0.06-0.1,-0.02-0.1,-0.06-0.1, 0.06-0.1,0.02-0.1,0.06-0.1},
-				--papyrus 2
-				{-0.03-0.4,-0.5,-0.03-0.3, 0.03-0.4,0.5,0.03-0.3},
-				{-0.06-0.4,-0.02-0.2,-0.06-0.3, 0.06-0.4,0.02-0.2,0.06-0.3},
-				--papyrus 3
-				{-0.03+0.4,-0.5,-0.03-0.3,0.03+0.4,0.5,0.03-0.3},
-				{-0.06+0.4,-0.02+0.2,-0.06-0.3, 0.06+0.4,0.02+0.2,0.06-0.3},
-				--papyrus 4
-				{-0.03-0.4,-0.5,-0.03+0.4, 0.03-0.4,0.5,0.03+0.4},
-				{-0.06-0.4,0.02+0.4,-0.06+0.4, 0.06-0.4,0.02+0.4,0.06+0.4},
-				--papyrus 5
-				{-0.03-0.2,-0.5,-0.03+0.2, 0.03-0.2,0.5,0.03+0.2},
-				{-0.06-0.2,0.02-0.4,-0.06+0.2, 0.06-0.2,0.02-0.4,0.06+0.2},
-				--papyrus 6
-				{-0.03+0.1,-0.5,-0.03+0.2, 0.03+0.1,0.5,0.03+0.2},
-				{-0.06+0.1,0.02+0.3,-0.06+0.2, 0.06+0.1,0.02+0.3,0.06+0.2},
-				},
-			}
+	type = "fixed",
+	fixed = {
+		--papyrus 1
+		{-0.03-0.1,-0.5,-0.03-0.1, 0.03-0.1,0.5,0.03-0.1},
+		{-0.06-0.1,-0.02-0.1,-0.06-0.1, 0.06-0.1,0.02-0.1,0.06-0.1},
+		--papyrus 2
+		{-0.03-0.4,-0.5,-0.03-0.3, 0.03-0.4,0.5,0.03-0.3},
+		{-0.06-0.4,-0.02-0.2,-0.06-0.3, 0.06-0.4,0.02-0.2,0.06-0.3},
+		--papyrus 3
+		{-0.03+0.4,-0.5,-0.03-0.3,0.03+0.4,0.5,0.03-0.3},
+		{-0.06+0.4,-0.02+0.2,-0.06-0.3, 0.06+0.4,0.02+0.2,0.06-0.3},
+		--papyrus 4
+		{-0.03-0.4,-0.5,-0.03+0.4, 0.03-0.4,0.5,0.03+0.4},
+		{-0.06-0.4,0.02+0.4,-0.06+0.4, 0.06-0.4,0.02+0.4,0.06+0.4},
+		--papyrus 5
+		{-0.03-0.2,-0.5,-0.03+0.2, 0.03-0.2,0.5,0.03+0.2},
+		{-0.06-0.2,0.02-0.4,-0.06+0.2, 0.06-0.2,0.02-0.4,0.06+0.2},
+		--papyrus 6
+		{-0.03+0.1,-0.5,-0.03+0.2, 0.03+0.1,0.5,0.03+0.2},
+		{-0.06+0.1,0.02+0.3,-0.06+0.2, 0.06+0.1,0.02+0.3,0.06+0.2},
+	},
+}
+
 local function papyrusnode(name, desc, img, drop)
 	minetest.register_node(name,{
 			description	=	desc,
@@ -40,47 +45,47 @@ local function papyrusnode(name, desc, img, drop)
 	})
 end
 
-papyrusnode(":default:papyrus", "Papyrus", "mod_papyrus.png", "default:papyrus")
-papyrusnode("rotten_papyrus:papyrus_rotten1", "", "mo_papyrus_rotten1.png", "default:papyrus")
-papyrusnode("rotten_papyrus:papyrus_rotten2", "", "mo_papyrus_rotten2.png", "")
+papyrusnode(":default:papyrus", "Papyrus", "rotten_papyrus.png", "default:papyrus")
+papyrusnode("rotten_papyrus:dried1", "", "rotten_papyrus_dried1.png", "default:papyrus")
+papyrusnode("rotten_papyrus:dried2", "", "rotten_papyrus_dried2.png", "default:paper")
+
+local function allow_papyrus(pos)
+	if minetest.find_node_near(pos, 2, {"default:water_source","default:water_flowing"})
+	or minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "default:papyrus" then
+		return true
+	end
+	return false
+end
 
 minetest.register_abm ({
-	nodenames = {"rotten_papyrus:papyrus_rotten1","rotten_papyrus:papyrus_rotten2"},
-	interval = 0,
-	chance = 1,
-	action = function (pos)
-		if minetest.env:get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "air" then
-			minetest.env:remove_node(pos)
-		end
-	end,
-})
-minetest.register_abm ({
-	nodenames = {"default:papyrus","rotten_papyrus:papyrus_rotten1","rotten_papyrus:papyrus_rotten2"},
+	nodenames = {"default:papyrus","rotten_papyrus:dried1","rotten_papyrus:dried2"},
 	interval = PAPYRUS_INTERVALL,
 	chance = PAPYRUS_CHANCE,
-	action = function (pos)
-		local env = minetest.env
-		local under = env:get_node({x=pos.x, y=pos.y-1, z=pos.z})
+	action = function (pos, node)
+		local nam = node.name
+		local papyrus_allowed = allow_papyrus(pos)
 
 ---------------fault
-		if env:get_node(pos).name == "default:papyrus" then
-			if env:find_node_near(pos, 1, {"default:water_source","default:water_flowing"})
-			or under.name == "default:papyrus" then return
-			else env: add_node (pos, {name = "rotten_papyrus:papyrus_rotten1"})
+		if nam == "default:papyrus" then
+			if not papyrus_allowed then
+				minetest.add_node(pos, {name = "rotten_papyrus:dried1"})
 			end
+			return
+		end
 
 ---------------fault mehr
-		elseif env:get_node(pos).name == "rotten_papyrus:papyrus_rotten1" then
-			if env:find_node_near(pos, 1, {"default:water_source","default:water_flowing"})
-			or under.name == "default:papyrus" then
-				env: add_node (pos, {name = "default:papyrus"})
+		if nam == "rotten_papyrus:dried1" then
+			if papyrus_allowed then
+				minetest.add_node(pos, {name = "default:papyrus"})
 			else
-				env: add_node (pos, {name = "rotten_papyrus:papyrus_rotten2"})
+				minetest.add_node(pos, {name = "rotten_papyrus:dried2"})
 			end
+			return
+		end
 
 ---------------stirbt ab
-		elseif env:get_node(pos).name == "rotten_papyrus:papyrus_rotten2" then
-			env:remove_node(pos)
-		end
+		minetest.remove_node(pos)
 	end,
 })
+
+print(string.format("[rotten_papyrus] loaded after ca. %.2fs", os.clock() - t1))
